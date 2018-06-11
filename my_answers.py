c1ff542c1e4012fec6fa1ac1e0d555c2d5e1aeac
Wan Li <wanderly0501@gmail.com>
2018-06-11 03:07:46 -0700

    first version

---
+++ my_answers.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+
+
+# TODO: fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series, window_size):
+    # containers for input/output pairs
+    X = []
+    y = []
+    for i in range(0, len(series)-window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
+
+    # reshape each 
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+
+    return X,y
+
+
+
+### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+def cleaned_text(text):
+    punctuation = ['!', ',', '.', ':', ';', '?', ' ']
+    letters='abcdefghijklmnopqrstuvwxyz'
+    newtext=''
+    for i in text:
+        if i in punctuation or i in letters:
+            newtext+=i
+        else:
+            newtext+=' '
+    return newtext
+
+### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_text(text, window_size, step_size):
+    # containers for input/output pairs
+    inputs = []
+    outputs = []
+    n=len(text)
+    for i in range(n)[::step_size]:
+        if i+window_size<n:
+            inputs.append(text[i:i+window_size])
+            outputs.append(text[i+window_size])
+
+    return inputs,outputs
+
